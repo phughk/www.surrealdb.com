e3960c50b7bc1ffd2b7517576c0a407ad1b92e51
ElecTwix <51170038+ElecTwix@users.noreply.github.com>
2023-04-17 21:29:04 +0300

    Update create.go Fix the TODO Comment and rewrite the Create

--- app/snippets/docs/integration/libraries/golang/create.go
+++ app/snippets/docs/integration/libraries/golang/create.go
@@ -1,11 +1,31 @@
-TODO THIS ISNT GOLANG
 // Create a record with a random ID
-let person = await db.create('person');
-// Create a record with a specific ID
-let record = await db.create('person:tobie', {
-	name: 'Tobie',
-	settings: {
-		active: true,
-		marketing: true,
+person := db.Create("person", map[string]interface{});
+			
+// Create a record with a specific ID w/ a map
+	await db.create("person:tobie", map[string]interface{}{
+	"name": "Tobie",
+	"settings": map[string]bool{
+		"active": true,
+		"marketing": true,
+	},
+});
+
+// Create a record with a specific ID w/ a struct
+type Person struct {
+	Name string
+	Settings struct {
+		Active bool
+		Marketing bool
+	}
+}
+
+await db.create("person:tobie", Person{
+	Name: "Tobie",
+	Settings: struct {
+		Active    bool
+		Marketing bool
+	}{
+		Active:    true,
+		Marketing: true,
 	},
 });
