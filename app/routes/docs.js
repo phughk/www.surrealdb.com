bc6976baaa9a3b84894931954eab972be7357f30
Micha de Vries <micha.de.vries@surrealdb.com>
2023-12-05 13:58:53 +0000

    name from intent as fallback (#415)

--- app/routes/docs.js
+++ app/routes/docs.js
@@ -2,16 +2,20 @@ import Route from '@ember/routing/route';
 import { inject } from '@ember/service';
 
 export default class extends Route {
-
 	@inject fastboot;
 
 	redirect() {
-
 		// Get the page path
-		let path = arguments[1].intent.url;
+		let path =
+			arguments[1].intent.url ??
+			'/' + arguments[1].intent.name.replaceAll('.', '/');
 
 		// Check if this is an index page
 		let index = arguments[1].to.name.endsWith('index');
+
+		if (index && !arguments[1].intent.url) {
+			path = path.slice(0, -6);
+		}
 
 		// The default location is the same
 		let location = `https://docs.surrealdb.com${path}`;
@@ -28,7 +32,5 @@ export default class extends Route {
 		} else {
 			window.location.replace(location);
 		}
-
 	}
-
 }
