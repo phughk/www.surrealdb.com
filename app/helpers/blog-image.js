afc4dc4f092d705364ff6941473674de26a83f08
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2024-01-25 10:23:06 +0000

    Use `avif` image format, and fall back to `webp` format

--- app/helpers/blog-image.js
+++ app/helpers/blog-image.js
@@ -3,15 +3,15 @@ import { helper } from '@ember/component/helper';
 export function image([id, type]) {
 	switch (type) {
 		case 'main':
-			return `https://cdn.brandsafe.io/w(1720)q(90)sharpen(1,0.4,0.8)/${id}.auto`;
+			return `https://cdn.brandsafe.io/w(1720)q(80)/${id}.auto`;
 		case 'post':
-			return `https://cdn.brandsafe.io/w(1600)q(90)sharpen(1,0.4,0.8)/${id}.auto`;
+			return `https://cdn.brandsafe.io/w(1600)q(80)/${id}.auto`;
 		case 'large':
-			return `https://cdn.brandsafe.io/w(1250)q(90)sharpen(1,0.4,0.8)/${id}.auto`;
+			return `https://cdn.brandsafe.io/w(1250)q(80)/${id}.auto`;
 		case 'small':
-			return `https://cdn.brandsafe.io/w(820)q(90)sharpen(1,0.4,0.8)/${id}.auto`;
+			return `https://cdn.brandsafe.io/w(820)q(80)/${id}.auto`;
 		default:
-			return `https://cdn.brandsafe.io/w(1600)q(90)sharpen(1,0.4,0.8)/${id}.auto`;
+			return `https://cdn.brandsafe.io/w(1600)q(80)/${id}.auto`;
 	}
 }
 
