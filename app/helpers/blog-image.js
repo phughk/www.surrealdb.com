52b96cdba7ea74a9b82b0095fe2ed3a01910ed78
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2024-01-27 07:58:30 +0000

    Use webp image path

--- app/helpers/blog-image.js
+++ app/helpers/blog-image.js
@@ -3,15 +3,15 @@ import { helper } from '@ember/component/helper';
 export function image([id, type]) {
 	switch (type) {
 		case 'main':
-			return `https://cdn.brandsafe.io/w(1720)q(80)/${id}.auto`;
+			return `https://cdn.brandsafe.io/w(1720)q(80)/${id}.webp`;
 		case 'post':
-			return `https://cdn.brandsafe.io/w(1600)q(80)/${id}.auto`;
+			return `https://cdn.brandsafe.io/w(1600)q(80)/${id}.webp`;
 		case 'large':
-			return `https://cdn.brandsafe.io/w(1250)q(80)/${id}.auto`;
+			return `https://cdn.brandsafe.io/w(1250)q(80)/${id}.webp`;
 		case 'small':
-			return `https://cdn.brandsafe.io/w(820)q(80)/${id}.auto`;
+			return `https://cdn.brandsafe.io/w(820)q(80)/${id}.webp`;
 		default:
-			return `https://cdn.brandsafe.io/w(1600)q(80)/${id}.auto`;
+			return `https://cdn.brandsafe.io/w(1600)q(80)/${id}.webp`;
 	}
 }
 
