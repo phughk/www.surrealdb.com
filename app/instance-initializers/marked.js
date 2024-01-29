6650f97e646f14e80a39ac42207e34eea06138b4
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2024-01-29 20:24:40 +0000

    Remove unused code highlighting languages

--- app/instance-initializers/marked.js
+++ app/instance-initializers/marked.js
@@ -23,38 +23,38 @@ export default {
 		marked.setOptions({
 			highlight(code, lang) {
 				switch (lang) {
-				case 'go':
-					return Prism.highlight(code, Prism.languages.go, 'go');
-				case 'groovy':
-					return Prism.highlight(code, Prism.languages.groovy, 'groovy');
-				case 'java':
-					return Prism.highlight(code, Prism.languages.java, 'java');
-				case 'javascript':
-					return Prism.highlight(code, Prism.languages.javascript, 'javascript');
-				case 'js':
-					return Prism.highlight(code, Prism.languages.js, 'js');
-				case 'json':
-					return Prism.highlight(code, Prism.languages.json, 'json');
-				case 'python':
-					return Prism.highlight(code, Prism.languages.python, 'python');
-				case 'rust':
-					return Prism.highlight(code, Prism.languages.rust, 'rust');
-				case 'rs':
-					return Prism.highlight(code, Prism.languages.rust, 'rust');
-				case 'shell':
-					return Prism.highlight(code, Prism.languages.shell, 'shell');
-				case 'sql':
-					return Prism.highlight(code, Prism.languages.sql, 'sql');
-				case 'surql':
-					return Prism.highlight(code, Prism.languages.surql, 'surql');
-				case 'docker':
-					return Prism.highlight(code, Prism.languages.docker, 'docker');
-				case 'toml':
-					return Prism.highlight(code, Prism.languages.toml, 'toml');
-				case 'yaml':
-					return Prism.highlight(code, Prism.languages.yaml, 'yaml');
-				default:
-					return code;
+					case 'bash':
+						return Prism.highlight(code, Prism.languages.bash, 'bash');
+					case 'go':
+						return Prism.highlight(code, Prism.languages.go, 'go');
+					case 'javascript':
+						return Prism.highlight(code, Prism.languages.javascript, 'javascript');
+					case 'js':
+						return Prism.highlight(code, Prism.languages.javascript, 'javascript');
+					case 'json':
+						return Prism.highlight(code, Prism.languages.json, 'json');
+					case 'python':
+						return Prism.highlight(code, Prism.languages.python, 'python');
+					case 'rs':
+						return Prism.highlight(code, Prism.languages.rust, 'rust');
+					case 'rust':
+						return Prism.highlight(code, Prism.languages.rust, 'rust');
+					case 'shell':
+						return Prism.highlight(code, Prism.languages.shell, 'shell');
+					case 'sql':
+						return Prism.highlight(code, Prism.languages.sql, 'sql');
+					case 'surql':
+						return Prism.highlight(code, Prism.languages.surql, 'surql');
+					case 'toml':
+						return Prism.highlight(code, Prism.languages.toml, 'toml');
+					case 'ts':
+						return Prism.highlight(code, Prism.languages.typescript, 'typescript');
+					case 'typescript':
+						return Prism.highlight(code, Prism.languages.typescript, 'typescript');
+					case 'yaml':
+						return Prism.highlight(code, Prism.languages.yaml, 'yaml');
+					default:
+						return code;
 				}
 			}
 		});
