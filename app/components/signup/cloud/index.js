9312866cd124532989a23db27f1437489afdef0a
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2024-03-14 16:24:53 +0000

    Add support for HubSpot forms (#508)

--- app/components/signup/cloud/index.js
+++ app/components/signup/cloud/index.js
@@ -1,47 +1,8 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
-import { drop } from '@ascua/tasks';
 
 export default class extends Component {
+
 	@tracked show;
 
-	@tracked done;
-
-	@tracked firstname;
-	@tracked lastname;
-	@tracked email;
-	@tracked jobtitle;
-	@tracked companyname;
-
-	@drop *submit(event) {
-		event.stopPropagation();
-
-		event.preventDefault();
-
-		if (!this.firstname) throw 'Please provide a first name';
-		if (!this.lastname) throw 'Please provide a last name';
-		if (!this.email) throw 'Please provide an email';
-
-		yield fetch('https://form.surrealdb.com/j7748et8593167h1hn9qyfbyi2', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({
-				firstname: this.firstname,
-				lastname: this.lastname,
-				email: this.email,
-				jobtitle: this.jobtitle,
-				companyname: this.companyname,
-			}),
-		});
-
-		this.firstname = null;
-		this.lastname = null;
-		this.email = null;
-		this.email = null;
-		this.jobtitle = null;
-		this.companyname = null;
-		this.done = true;
-	}
 }
--- app/components/signup/newsletter/index.js
+++ app/components/signup/newsletter/index.js
@@ -1,44 +1,8 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
-import { drop } from '@ascua/tasks';
 
 export default class extends Component {
 
 	@tracked show;
-
-	@tracked done;
-
-	@tracked firstname;
-	@tracked lastname;
-	@tracked email;
-
-	@drop * submit(event) {
-
-		event.stopPropagation();
-
-		event.preventDefault();
-
-		if (!this.firstname) throw 'Please provide a first name';
-		if (!this.lastname) throw 'Please provide a last name';
-		if (!this.email) throw 'Please provide an email';
-
-		yield fetch('https://form.surrealdb.com/b1167yg1864664e2nv8shudji2', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({
-				firstname: this.firstname,
-				lastname: this.lastname,
-				email: this.email,
-			}),
-		});
-
-		this.firstname = null;
-		this.lastname = null;
-		this.email = null;
-		this.done = true;
-
-	}
 
 }
