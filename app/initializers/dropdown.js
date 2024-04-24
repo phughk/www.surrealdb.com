ffbd66b6b42619347b4036fb895d795831b21168
Micha de Vries <micha@devrie.sh>
2024-04-24 13:46:00 +0100

    Force close dropdown on clicking link (#532)

---
+++ app/initializers/dropdown.js
@@ -0,0 +1,19 @@
+export default {
+	name: 'dropdown',
+
+	initialize() {
+		if (typeof window !== 'undefined' && !window.dropdownReady) {
+			window.dropdownReady = true;
+			if (typeof window.navigation !== 'undefined') {
+				window.navigation.addEventListener("navigate", (event) => {
+					const els = Array.from(document.querySelectorAll(".dropdown, .dropdown-background"));
+					els.map((el) => {
+						const curr = el.style.display
+						el.style.display = 'none';
+						setTimeout(() => el.style.display = curr, 1)
+					});
+				});
+			}
+		}
+	},
+};
