5a9e2da270802be1c5a52c59450f5445b01bd8bf
Jaime Morgan Hitchcock <jaime@surrealdb.com>
2024-04-24 14:43:59 +0100

    Fix dropdown js script

--- app/initializers/dropdown.js
+++ app/initializers/dropdown.js
@@ -6,11 +6,11 @@ export default {
 			window.dropdownReady = true;
 			if (typeof window.navigation !== 'undefined') {
 				window.navigation.addEventListener("navigate", (event) => {
-					const els = Array.from(document.querySelectorAll(".dropdown, .dropdown-background"));
+					const els = Array.from(document.querySelectorAll(".dropdown, .dropdown-background, .dropdown-backdrop"));
 					els.map((el) => {
 						const curr = el.style.display
 						el.style.display = 'none';
-						setTimeout(() => el.style.display = curr, 1)
+						setTimeout(() => el.style.display = curr, 500)
 					});
 				});
 			}
